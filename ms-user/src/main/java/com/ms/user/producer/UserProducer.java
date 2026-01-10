e2ed92ea6dbc5767baa2a30ef3452c40029aeb74
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2026-01-10 15:37:02 -0300

    feat: implement UserProducer for sending messages to RabbitMQ

--- ms-user/src/main/java/com/ms/user/producer/UserProducer.java
+++ ms-user/src/main/java/com/ms/user/producer/UserProducer.java
@@ -1,4 +1,34 @@
 package com.ms.user.producer;
 
+import com.ms.user.config.RabbitMQConfig;
+import com.ms.user.dto.EmailRequestDto;
+import com.ms.user.dto.UserRequestDTO;
+import com.ms.user.model.User;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserProducer {
+
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value(value = "${broker.queue.email.name}")
+    private String routingKey;
+
+    public void publishMessageEmail(User user){
+        EmailRequestDto emailRequestDto = new EmailRequestDto(
+                user.getUserId(),
+                user.getEmail(),
+                "Cadastro realizado",
+                user.getName() + "Seja Bem-Vindo!"
+        );
+
+        rabbitTemplate.convertAndSend("", routingKey, emailRequestDto);
+
+    }
 }
