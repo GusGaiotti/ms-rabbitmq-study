dc6173f96cfb2392c57ebf3fa55b4e92c43eb9bc
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2026-01-11 20:01:02 -0300

    feat: implement email message consumer

--- ms-email/src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ ms-email/src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -1,16 +1,28 @@
 package com.ms.email.consumer;
 
 import com.ms.email.dto.EmailRequestDTO;
+import com.ms.email.model.Email;
+import com.ms.email.service.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    private final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRequestDTO emailRequestDTO){
-        System.out.println(emailRequestDTO.emailTo());
+        Email email = new Email();
+        BeanUtils.copyProperties(emailRequestDTO, email);
+
+        emailService.sendEmail(email);
     }
 
 }
